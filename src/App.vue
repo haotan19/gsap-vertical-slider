7d373f48aa51f7f642343bb04599ed6cd7c5088b
Hao Tan <80441177+haotan19@users.noreply.github.com>
2022-06-02 12:28:39 +0800

    Project init

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Slide from "./components/slide.vue";
+import { gsap } from "gsap";
+import { onMounted, reactive } from "vue";
+
+const slideData = [
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
+];
+
+const slidesPositions = reactive({
+  value: [-2, -1, 0, 1, 2]
+})
+
+onMounted(() => {
+  // gsap.fromTo
+});
+</script>
+
+<template>
+  <div class="">
+    <Slide
+      :key="slideData[0].id"
+      :idx="0"
+      :pos="slidesPositions.value[2]"
+    ></Slide>
+    <!-- <Slide
+      v-for="(slide, idx) in slideData"
+      :key="slide.id"
+      :idx="idx"
+      :pos="-Math.floor((slideData.length) / 2) + idx"
+    ></Slide> -->
+  </div>
+  <button >Prev</button>
+  <button class="absolute right-0">NEXT</button>
+</template>
+
+<style></style>
---
+++ src/components/slide.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+    pos: number,
+    idx: number
+}>()
+</script>
+
+<template>
+<!-- if POS is 0, set it to screen center -->
+    <div class="absolute left-1/2 -translate-x-1/2  w-[300px] h-[500px] bg-slate-300">IDX: {{ idx }} Slide POS: {{ pos }}</div>
+</template>
